164060468f1a21f3be840775ae96a1836ba9f594
Shrihari <shrihari1999@gmail.com>
2022-12-10 19:56:56 +0530

    auth optional

--- lalamove/auth.py
+++ lalamove/auth.py
@@ -1,7 +1,8 @@
+from lalamove.models.internal_types import Optional
 from .config import Config
 import hashlib, hmac
 
-def signRequest(config: Config, method: str, path: str, timestamp: int, body: str) -> str:
+def signRequest(config: Config, method: str, path: str, timestamp: int, body: Optional[str]) -> str:
     rawSignature = str(timestamp) + '\r\n' + method + '\r\n' + path +  '\r\n\r\n'
     if body:
         rawSignature += body
